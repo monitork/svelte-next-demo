de49c982afa815d7592527ec4d3aa21ff25922fb
Nguyen Duy Toai <nguyenduytoai@NDT-MBP.local>
2020-12-24 23:13:19 +0700

    Some config layout

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+// Consult https://www.snowpack.dev to learn about these options
+module.exports = {
+	extends: '@sveltejs/snowpack-config',
+	plugins: [[
+		"@snowpack/plugin-svelte",
+		{
+			compilerOptions: {
+				hydratable: true
+			}
+		}
+	],
+	'@snowpack/plugin-typescript'],
+	mount: {
+		'src/components': '/_components'
+	},
+	alias: {
+		$components: './src/components'
+	}
+};
---
+++ src/routes/$layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href=".">Home</a>
+	<a href="about">About</a>
+	<a href="settings">Settings</a>
+</nav>
+<main><slot></slot></main>
